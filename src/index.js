2d7f8685976f0c8445bdf9a124447bfaae938f0f
Krystof Rainisch <Krystof.Rainisch@hey.com>
2021-07-14 14:37:14 +0200

    add webhook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 const getNetlify = require('./getNetlify')
+const fetch = require('node-fetch')
 
 const app = express()
+app.use(bodyParser.json())
+
 const CACHE_TIME = 1000 * 5 // 5s
+
+const { IFTTT_KEY } = process.env
+const iftttWebhookUrl = `https://maker.ifttt.com/trigger/letna_dev_notification/with/key/${IFTTT_KEY}`
 
 let cache, timestamp
 
@@ -20,5 +28,21 @@ app.get('/', async function (req, res) {
     res.send(net)
   }
 })
+
+app.post('/webhook', async function (req, res) {
+  const response = await fetch(iftttWebhookUrl, {
+    method: 'POST', 
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "value1": req.body || "empty",
+    })
+  }).then(res => res.json())
+  .catch(()=>({"message":""}))
+  res.send(response)
+})
+
 
 app.listen(8081)
